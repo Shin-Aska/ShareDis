620f63f85e090b1034aa8264d398d8e395ceeeaa
Richard Orilla <Orillarichard@pm.me>
2023-12-17 12:46:22 +0800

    Add functionality for transformers and some changed  fillFormatStructure to be asynchronous

--- functionality.js
+++ functionality.js
@@ -24,6 +24,31 @@ async function getFormat() {
     })
 }
 
+async function getTransformer() {
+    return new Promise((resolve, reject) => {
+        browser.storage.local.get([`transformers`], (r) => {
+            if (`transformers` in r) {
+                let result = JSON.parse(r.format);
+                Object.keys(result).forEach((domain) => {
+                    try {
+                        result[domain].regex = new RegExp(result[domain].regex);
+                    }
+                    catch (ex) {
+                        console.warn("Exception occured while fetching RegExp " + ex);
+                    }
+                });
+                resolve(result);
+            }
+            else {
+                resolve({
+                    "www.youtube.com": {"regex": /^(?:https?:\/\/)?(?:www\.)?youtube\.com\/shorts\/([^\/?]+)$/i, "output": "https://www.youtube.com/watch?v=$1"},
+                    "m.youtube.com": {"regex": /^(?:https?:\/\/)?(?:m\.)?youtube\.com\/shorts\/([^\/?]+)$/i, "output": "https://www.youtube.com/watch?v=$1"}
+                });
+            }
+        })
+    })
+}
+
 function convertFormatToTagify(format) {
     let result = ``;
     for (var i = 0; i < format.length; i++) {
@@ -55,14 +80,30 @@ function validateFormat(format) {
     return hasTitle && hasURL;
 }
 
-function fillFormatStructure(url, title, format) {
+function getHostOfURL(url) {
+    let anchor = document.createElement('a');
+    anchor.href = url;
+    return anchor.hostname;
+}
+
+async function fillFormatStructure(url, title, format) {
     for (let i = 0; i < format.length; i++) {
         let structure = format[i];
         if (format[i].type === `title`) {
             format[i].value = title;
         }
         else if (format[i].type === `url`) {
-            format[i].value = url;
+            let hostname = getHostOfURL(url);
+            let transformers = await getTransformer();
+            if (hostname in transformers) {
+                let regex = transformers[hostname].regex;
+                let output = transformers[hostname].output;
+                console.log(transformers[hostname]);
+                format[i].value = url.replace(regex, output);
+            }
+            else {
+                format[i].value = url;
+            }
         }
     }
     return format;
