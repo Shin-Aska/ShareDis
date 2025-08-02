eddc703505ba1ee6de8e41f49218088d85bf91d6
Richard Orilla <Shin-Aska@users.noreply.github.com>
2025-08-02 23:24:18 +0800

    Merge pull request #6
    * Add transformer table feature allowing better edits with transformer feature

--- transformer.js
+++ transformer.js
@@ -1,8 +1,19 @@
+browser._userValues = {
+    host: null
+}
+
+function setOverwrittenHost(hostname) {
+    browser._userValues.host = hostname;
+}
+
+function getOverwrittenHost() {
+    return browser._userValues.host;
+}
+
 async function getHostOfCurrentTab() {
     // Get the page title and URL
     return new Promise((resolve, reject) => {
         browser.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
-            console.log(tabs);
             let tab = tabs[0];
             const pageURL = tab.url;
             resolve(getHostOfURL(pageURL));
@@ -10,24 +21,30 @@ async function getHostOfCurrentTab() {
     });
 }
 
+async function updateTransformerInput(hostname) {
+    document.getElementById(`transformer-hostname`).innerHTML = hostname;
+    getTransformer(hostname).then((transformer) => {
+        document.getElementById(`transformer-controller-regex`).value = transformer.regex.toString();
+        document.getElementById(`transformer-controller-output`).value = transformer.output;
+    }).catch((err) => {
+
+    })
+}
+
 async function loadTransformerInputs() {
     getHostOfCurrentTab().then(async (hostname) => {
-        getTransformer(hostname).then((transformer) => {
-            document.getElementById(`transformer-controller-regex`).value = transformer.regex.toString();
-            document.getElementById(`transformer-controller-output`).value = transformer.output;
-        }).catch((err) => {
-
-        })
+        updateTransformerInput(hostname);
     })
 }
 
 document.getElementById(`applyTransformerBtn`).onclick = async () => {
-    let hostname = await getHostOfCurrentTab();
+    let hostname = getOverwrittenHost() || await getHostOfCurrentTab();
     let regex = RegExpFromStr(document.getElementById(`transformer-controller-regex`).value);
     let output = document.getElementById(`transformer-controller-output`).value;
     let updated_transformers = await updateTransformer(hostname, regex, output);
     browser.storage.local.set({transformers: JSON.stringify(updated_transformers)}, async () => {
         loadTransformerInputs();
+        await loadDataTable();
         showModal(`Transformer saved`, [`The transformer you have set for ${hostname} has been ` +
                 `applied. The next output for ${hostname} will be transformed.`], `success`);
     });
